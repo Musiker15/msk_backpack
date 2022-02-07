7263aee5a4fbfc78d93a174147a046bc7981e1a1
Musiker15 <info@musiker15.de>
2022-02-08 00:49:39 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
-----------------------------------------------------------------
+---------------------------------------------------------------
 Config.Locale = 'de'
 Config.VersionChecker = true
-----------------------------------------------------------------
+---------------------------------------------------------------
 Config.BagInventory = false -- Set true if you don't want the pocket inventory but want to expand the player inventory
 Config.BagWeight = 50 -- Set the Bag Weight if 'Config.BagInventory = true'
