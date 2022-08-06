e1d8771808dd6ee744c1182117fe0af0a47679ec
Musiker15 <info@musiker15.de>
2022-08-06 20:18:38 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -242,13 +242,13 @@ local CurrentVersion = GetCurrentVersion()
 local resourceName = "^4["..GetCurrentResourceName().."]^0"
 
 if Config.VersionChecker then
-	PerformHttpRequest('https://raw.githubusercontent.com/Musiker15/esx_backpack/main/VERSION', function(Error, NewestVersion, Header)
+	PerformHttpRequest('https://raw.githubusercontent.com/Musiker15/msk_backpack/main/VERSION', function(Error, NewestVersion, Header)
 		print("###############################")
     	if CurrentVersion == NewestVersion then
 	    	print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
     	elseif CurrentVersion ~= NewestVersion then
         	print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
-	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://github.com/Musiker15/esx_backpack/releases/tag/v'.. NewestVersion .. '^0')
+	    	print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://github.com/Musiker15/msk_backpack/releases/tag/v'.. NewestVersion .. '^0')
     	end
 		print("###############################")
 	end)
