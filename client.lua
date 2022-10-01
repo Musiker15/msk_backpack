e4eb7712e0b74f835977db53872f332388cb8f2c
Musiker15 <info@musiker15.de>
2022-10-01 18:53:03 +0200

    Update
    * Updated support for ESX Legacy

--- client.lua
+++ client.lua
@@ -1,10 +1,4 @@
-ESX = nil
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
+ESX = exports["es_extended"]:getSharedObject()
 
 local currentBag, currentBagWeight = nil, nil
 
--- server.lua
+++ server.lua
@@ -1,5 +1,4 @@
-local ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
 
 local setDebug = false
 
