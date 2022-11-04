be0f70e6d7d1607528a189bf6513f346874c70e7
Musiker15 <info@musiker15.de>
2022-11-04 14:35:14 +0100

    Create pl.lua

---
+++ locales/pl.lua
@@ -0,0 +1,19 @@
+Locales ['pl'] = {
+    ['used_bag'] = 'Założyłes torbę',
+    ['used_nobag'] = 'Odłożyłeś torbę',
+    ['has_bag'] = 'Masz już torbę',
+    ['had_bag'] = 'Odłożyłeś już torbę',
+    ['noBag'] = 'Potrzebujesz torby aby użyć tej broni',
+    ['otherBag'] = 'Potrzebujesz inną torbę',
+    ['itemsInBag'] = 'Musisz wyciągnąć przedmioty z torby aby wyrzucić je na ziemię',
+
+    ['youre_dead'] = 'Jesteś martwy mordo, więc dlaczego próbujesz używać torby ?',
+    ['someone_searching'] = ' przeszukujesz torbę',
+    ['you_searching'] = 'Przeszukujesz torbę: ',
+    ['not_handsup'] = 'Obywatel się nie poddał',
+    ['player_noBag'] = 'Obywatel nie ma torby!',
+    ['no_player_nearby'] = 'Brak Obywateli w pobliżu!',
+
+    ['entity_dead'] = 'Koleś nie żyje!',
+    ['too_heavy'] = 'Masz pełne kieszenie, nie możesz zdjąć torby',
+}
